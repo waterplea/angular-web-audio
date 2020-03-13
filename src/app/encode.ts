46a2bdb5ce7fea6a37cc6dbcd33f0cb21b0863ea
waterplea <alexander@inkin.ru>
2020-03-13 15:37:56 +0300

    1 file modified, 2 added

---
+++ src/app/encode.ts
@@ -0,0 +1,11 @@
+function decode(base64: string): ArrayBuffer {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array(len);
+
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+
+  return bytes.buffer;
+}
